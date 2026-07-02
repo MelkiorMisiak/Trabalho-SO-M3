b9516a3655987b9730438ebb6ef37495613201a0
MelkiorMisiak <misiakmelkior@gmail.com>
2026-07-01 23:09:10 -0300

    Implemented block.c but didn't test it

--- drivers/block.c
+++ drivers/block.c
@@ -1,5 +1,14 @@
 #include "block.h"
 
-int block_read(uint32_t block, void *buffer);
-int block_write(uint32_t block, const void *buffer);
+int block_read(uint32_t block, void *buffer)
+{
+	uint8_t *value = buffer;
+	*value = disk[block];
+}
+
+int block_write(uint32_t block, const void *buffer)
+{
+	const uint8_t *value = buffer;
+	disk[block] = *value;
+}
 
